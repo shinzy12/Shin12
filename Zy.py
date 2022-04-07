2eb09d53b46024d9f2bca7009b61a54e4773d1a9
shinzy12 <91730481+shinzy12@users.noreply.github.com>
2022-04-08 05:23:05 +0700

    Update Zy.py

--- Zy.py
+++ Zy.py
@@ -76,7 +76,7 @@ def banner():
 	wel = '# WELCOME TO SHINXZY TOOLS'
 	wel2 = mark(wel, style='cyan')
 	sol().print(wel2)
-	print(f""" {h}
+	print(f""" {H}
    _______  _____   _____   _______  ______  ______  -
     |      |     |  |    |     |     |     | |     | |
     |_____ |_____|  |    |  °° |     |_____| |_____| |____
