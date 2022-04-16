34a85c88faeab757de361f9b535ce445d09aa485
shinzy12 <91730481+shinzy12@users.noreply.github.com>
2022-04-16 08:13:58 +0700

    Update Zy.py

--- Zy.py
+++ Zy.py
@@ -93,10 +93,10 @@ def login():
 			tokenku.append(token)
 			try:
 				sy = requests.get('https://graph.facebook.com/me?access_token='+tokenku[0])
-    		        	sy2 = json.loads(sy.text)['name']
+				sy2 = json.loads(sy.text)['name']
 				sy3 = json.loads(sy.text)['id']
 				sy4 = json.loads(sy.text)['birthday']
-				menu(,sy2,sy3,sy4)
+				menu(sy2,sy3,sy4)
 			except KeyError:
 				login_lagi()
 			except requests.exceptions.ConnectionError:
