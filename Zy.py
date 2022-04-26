a6bfee6ec94e7ca1c1e5771708459e20240e667d
shinzy12 <91730481+shinzy12@users.noreply.github.com>
2022-04-27 02:40:11 +0700

    Update Zy.py

--- Zy.py
+++ Zy.py
@@ -504,7 +504,7 @@ def passwrd():
 		for yuzong in id2:
 			idf,nmf = yuzong.split('|')[0],yuzong.split('|')[1].lower()
 			frs = nmf.split(' ')[0]
-			pwv = ['123456','cintaku','terserah','surabaya','rahasia','indonesia','jancok','kontol','jancok123','kontol123','sayang','sayangku','sayang123','bismillah','anjing','katasandi','sandi123']
+			pwv = ['memek123','cintasuci','cintaku','terserah','surabaya','rahasia','indonesia','jancok','kontol','jancok123','kontol123','sayang','sayangku','sayang123','bismillah','anjing','katasandi','sandi123','anjing123']
 			if len(nmf)<6:
 				if len(frs)<3:
 					pass
