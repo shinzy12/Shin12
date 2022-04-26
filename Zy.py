a60fde954586367b515d52dda80deae957bea124
shinzy12 <91730481+shinzy12@users.noreply.github.com>
2022-04-27 06:55:31 +0700

    Update Zy.py

--- Zy.py
+++ Zy.py
@@ -499,7 +499,6 @@ def passwrd():
 	ler = '# CRACK PROCESS BEGINS, PRESS CTRL+Z TO STOP'
 	sol().print(mark(ler, style='purple'))
 	krek = 'Hasil OK  Disimpan Ke : OK/%s\nHasil CP Disimpan Ke : CP/%s\nMode Pesawat Setiap 5 Menit, jika Masih tidak ada hasil, silahkan banting hp mu lalu beli hp baru, ojo di pek batine ae Gak gelem ganti HP'%(okc,cpc)
-        gess = nel(krek, style='purple')
 	cetak(nel(krek, title='CRACK'))
 	with tred(max_workers=30) as pool:
 		for yuzong in id2:
