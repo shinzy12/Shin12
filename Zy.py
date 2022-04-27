468eaa2f6b8c0e547d2e1cbe57e2a9216aedfcd6
shinzy12 <91730481+shinzy12@users.noreply.github.com>
2022-04-27 07:01:02 +0700

    Update Zy.py

--- Zy.py
+++ Zy.py
@@ -155,7 +155,7 @@ def menu(my_name,my_id):
 	print(x+'['+h+'•'+x+'] \033[1;35mID Tumbal     : '+str(my_id))
 	print(x+'['+h+'•'+x+'] \033[1;93mTanggal Lahir : '+str(birth))
 	print(x+'['+h+'•'+x+'] \033[1;93mAlamat Ip     : '+str(sh['origin']))
-	io = '\x1b[01] Crack ID Pertemanan/Publik\n\x1b[02] Crack ID Publik (Masal) \n\x1b[03] Cek Hasil Crack\n\x1b[04] Cek Opsi Checkpoint\n\[00] METU BANGSAT'
+	io = '\x1b [01] Crack ID Pertemanan/Publik\n\x1b [02] Crack ID Publik (Masal) \n\x1b [03] Cek Hasil Crack\n\x1b [04] Cek Opsi Checkpoint\n [00] METU BANGSAT'
 	oi = nel(io, style='purple')
 	cetak(nel(oi, title='PILIHAN MENU'))
 	jh = input(x+'['+p+'•'+x+'] Pilih : ')
